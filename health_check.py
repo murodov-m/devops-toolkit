16a475a6a53635ca18b773b7663e3fa0b3cea728
murodov-m <mamur.m@gmail.com>
2026-05-06 17:22:49 +0500

    Initial commit

---
+++ health_check.py
@@ -0,0 +1,10 @@
+import psutil, datetime
+
+def check_system():
+	print(f"[{datetime.datetime.now()}] System Health Report")
+	print(f"CPU Usage:		{psutil.cpu_percent(interval=1)}%")
+	print(f"RAM Usage: 		{psutil.virtual_memory().percent}%")
+	print(f"Disk Usage: 	{psutil.disk_usage('/').percent}%")
+
+if __name__ == "__main__":
+	check_system()
